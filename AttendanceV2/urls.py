3ba44b6a8dcb7102a34468b23b4345327427c51c
PJ Reiniger <pj.reiniger@gmail.com>
2025-01-08 22:17:15 -0500

    Lint

--- AttendanceV2/urls.py
+++ AttendanceV2/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("attendance/", include("attendance.urls")),
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
