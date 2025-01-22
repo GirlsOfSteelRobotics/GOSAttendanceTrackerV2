ad46b7ad63b87364aba687efd4535c43017daf35
Sarah Withee <2601974+geekygirlsarah@users.noreply.github.com>
2025-01-21 21:26:00 -0500

    Add in Postgres support + load in DATABASE_URL from env

--- AttendanceV2/settings.py
+++ AttendanceV2/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,7 +81,9 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
+# Update database info if it's in $DATABASE_URL environment variable
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
