4eac258402b147675240904e95cf3f3de4e1f077
PJ Reiniger <pj.reiniger@gmail.com>
2025-08-24 19:07:05 -0400

    Add 2025-26 models

--- attendance/models/__init__.py
+++ attendance/models/__init__.py
@@ -1,11 +1,13 @@
-from attendance.models.y2025.gos import GosProgram2025, GosSubteam2025
+from attendance.models.y2025.field_builders import *  # noqa
+from attendance.models.y2025.gos import *  # noqa
+from attendance.models.y2025.scra import *  # noqa
+from attendance.models.y2026.field_builders import *  # noqa
+from attendance.models.y2026.gos import *  # noqa
+from attendance.models.y2026.scra import *  # noqa
 
 year = 2025
 
 if year == 2025:
-    from attendance.models.y2025.field_builders import *  # noqa
-    from attendance.models.y2025.gos import *  # noqa
-    from attendance.models.y2025.scra import *  # noqa
 
     GosStudent = GosStudent2025
     GosAttendance = GosAttendance2025
@@ -18,3 +20,15 @@ if year == 2025:
     GosSubteam = GosSubteam2025
     GosGradeLevel = GosGradeLevel2025
     GosPreseasonCrew = GosPreseasonCrew2025
+elif year == 2026:
+    GosStudent = GosStudent2026
+    GosAttendance = GosAttendance2026
+    ScraVisitorAttendance = ScraVisitorAttendance2026
+    ScraVisitor = ScraVisitor2026
+    FieldBuilderAttendance = FieldBuilderAttendance2026
+    FieldBuilder = FieldBuilder2026
+
+    GosProgram = GosProgram2026
+    GosSubteam = GosSubteam2026
+    GosGradeLevel = GosGradeLevel2026
+    GosPreseasonCrew = GosPreseasonCrew2026
---
+++ attendance/models/y2026/field_builders.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils import timezone
+
+from attendance.models.mixins import InOutTimeMixin, AttendanceMixin
+
+
+class FieldBuilder2026(models.Model, InOutTimeMixin):
+    full_name = models.CharField(max_length=100)
+    forms_completed = models.BooleanField(default=False)
+
+    def __str__(self):  # pragma: no cover
+        return self.full_name
+
+    def _get_attendance_set(self):
+        return self.fieldbuilderattendance2026_set
+
+    def _log_in(self):
+        return FieldBuilderAttendance2026.objects.create(
+            field_builder=self, time_in=timezone.now()
+        )
+
+    def _full_name(self):
+        return self.full_name
+
+
+class FieldBuilderAttendance2026(AttendanceMixin):
+    field_builder = models.ForeignKey(FieldBuilder2026, on_delete=models.CASCADE)
+
+    def __str__(self):  # pragma: no cover
+        return f"{self.field_builder.full_name}: {self.time_in} - {self.time_out}"
